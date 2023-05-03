360814549b1cb3778eb9c94853f5802dbb851b56
Angelique Rivera <angeliqueriveramorales@gmail.com>
2023-05-02 20:05:46 -0400

    initial commit

---
+++ client/src/main.tsx
@@ -0,0 +1,13 @@
+require("dotenv").config();
+console.log(process.env); // remove this after you've confirmed it is working
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
