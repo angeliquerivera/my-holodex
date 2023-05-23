ab477b40715e593e2d2f90f8bc4dc3d077aed387
Angelique Rivera <angeliqueriveramorales@gmail.com>
2023-05-23 00:25:54 -0400

    refactor(server): add code to server.mjs to connect to DB

--- server/src/server.mjs
+++ server/src/server.mjs
@@ -4,6 +4,12 @@ import "./loadEnvironment.mjs";
 import records from "./routes/record.mjs";
 
 const PORT = process.env.PORT || 5050;
+const express = require("express");
+const mongoose = require("mongoose");
+
+const idolRouter = require("./routes/idolRouter");
+const idols = require("./__test__/test_idols");
+
 const app = express();
 
 app.use(cors());
@@ -21,6 +27,7 @@ mongoose.connect(
     console.log("Connected to HoloDex DB");
   },
   (err) => {
+    console.error("holodex DB error:");
     console.error(err);
   }
 );
